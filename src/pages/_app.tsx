9682ddfa88e1a6682bbba6648f2aacab7395eaaf
homer <homer@osquarecorp.com>
2022-10-05 13:43:05 +0900

    chore: 개발 환경 추가 세팅
    - eslint 오류 해결
    - 일부 설정 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'src/styles/GlobalStyle';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
-			<GlobalStyle />
-			<Component {...pageProps} />
-		</>
-	);
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import styled from 'styled-components';
 
 const A = styled.div`
-	height: 500px;
-	width: 500px;
-	background: black;
+  height: 500px;
+  width: 500px;
+  background: black;
 `;
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<div>hello world</div>
-			<A>df</A>
-		</>
-	);
+  return (
+    <>
+      <div>hello world</div>
+      <A>df</A>
+    </>
+  );
 };
 
 export default Home;
