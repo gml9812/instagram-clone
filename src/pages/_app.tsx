b809be636a711b68f883079232f5b618913aee9c
devSominPark <min85472@gmail.com>
2022-10-25 15:15:23 +0900

    chore: apollo 초기 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { GlobalStyle } from 'src/styles/GlobalStyle';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { authLink, httpLink } from '@apollo/links';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   );
 };
