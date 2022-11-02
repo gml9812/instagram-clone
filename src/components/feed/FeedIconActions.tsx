1bdfe253764d97b1e11de7c15a486a070cb457da
devSominPark <min85472@gmail.com>
2022-11-03 08:23:10 +0900

    feat: 좋아요 icon에 Props 추가

--- src/components/feed/FeedIconActions.tsx
+++ src/components/feed/FeedIconActions.tsx
@@ -1,46 +1,100 @@
-import React, { ReactElement } from 'react';
+import React, { useState } from 'react';
 import { Box, IconButton } from '@mui/material';
 import LikeIcon from '@icons/LikeIcon';
 import CommentIcon from '@icons/CommentIcon';
 import COLOR from '@styles/colors';
 import TextButton from '@components/template/TextButton';
+import { useRouter } from 'next/router';
+import { useMutation } from '@apollo/client';
+import { CREATE_LIKE } from '@queries/post';
+import { setAccessToken } from '@libs/token';
+import { parseCookies } from 'nookies';
+import { CookiesName } from '@libs/values';
+import { REFRESH_ATOKEN_MUTATION } from '@queries/auth';
 
 interface Props {
+  id: number;
   isLike: boolean;
   likeCount: number;
 }
 
-const FeedActions = ({ isLike, likeCount }: Props): ReactElement => {
+const FeedIconActions = ({ id, isLike, likeCount }: Props) => {
+  const router = useRouter();
+  const cookies = parseCookies();
+  const refreshToken = cookies[CookiesName.refreshToken];
+  const [refreshAToken] = useMutation<{ getATokenByRToken: string }>(
+    REFRESH_ATOKEN_MUTATION,
+    {
+      context: {
+        headers: {
+          'R-TOKEN': refreshToken,
+        },
+      },
+    },
+  );
+
+  const [likeState, setLikeState] = useState<{
+    isLike: boolean;
+    count: number;
+  }>({ isLike, count: likeCount });
+
+  const [createLike] = useMutation<{ createLike: boolean }>(CREATE_LIKE, {
+    variables: { likeInput: { itemId: id, type: 'POST' } },
+  });
+
+  const handleClickLike = async () => {
+    if (!likeState.isLike) {
+      setLikeState({ isLike: true, count: likeCount + 1 });
+      try {
+        createLike();
+      } catch {
+        const result = await refreshAToken();
+        setAccessToken(result.data?.getATokenByRToken || '');
+      }
+    }
+  };
+
+  const handleClickComment = () => {
+    router.push(`/post/${id}`);
+  };
+
   return (
     <>
       <Box sx={{ padding: '4px 8px' }}>
-        <IconButton sx={{ padding: '8px', margin: '0 4px 0 0' }}>
-          <LikeIcon isLike={isLike} />
+        <IconButton
+          sx={{ padding: '8px', margin: '0 4px 0 0' }}
+          onClick={handleClickLike}
+        >
+          <LikeIcon
+            isLike={likeState.isLike}
+            strokeColor={undefined}
+            strokeWidth={undefined}
+          />
         </IconButton>
-        <IconButton sx={{ padding: '8px' }}>
+        <IconButton sx={{ padding: '8px' }} onClick={handleClickComment}>
           <CommentIcon />
         </IconButton>
       </Box>
 
-      {likeCount !== 0 && (
+      {likeState.count !== 0 && (
         <TextButton
           sx={{
             display: 'flex',
             justifyContent: 'start',
             padding: 0,
             margin: '0 16px 2px',
-            fontSize: 13,
-            fontWeight: 400,
+            fontSize: '1.1rem',
+            fontWeight: 500,
             color: COLOR.CHARCOAL,
             lineHeight: '20px',
+            letterSpacing: '-0.02em',
           }}
         >
-          좋아요
-          <Box sx={{ margin: '0 0 0 4px', fontWeight: 700 }}>{likeCount}</Box>개
+          {`좋아요 ${likeState.count}개`}
         </TextButton>
       )}
     </>
   );
 };
 
-export default FeedActions;
+export default FeedIconActions;
--- src/icons/LikeIcon.tsx
+++ src/icons/LikeIcon.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 interface Props {
   isLike: boolean;
+  strokeColor: string | undefined;
+  strokeWidth: string | undefined;
 }
 
-const Like = ({ isLike }: Props) => {
+const Like = ({ isLike, strokeColor, strokeWidth }: Props) => {
   return (
     <svg
       width="20"
@@ -16,8 +18,8 @@ const Like = ({ isLike }: Props) => {
     >
       <path
         d="M17.5263 2.4309L17.5273 2.43188C19.4921 4.35933 19.4885 7.33453 17.531 9.24587L17.5308 9.24602L10.0003 16.6021L2.4697 9.24602L2.46955 9.24587C0.511576 7.33408 0.508839 4.35888 2.46921 2.4358L2.46921 2.4358L2.47068 2.43435C2.92995 1.98195 3.47755 1.62151 4.08257 1.37489C4.68746 1.12834 5.33707 1.00074 5.99385 1C7.15455 1.0002 8.35664 1.58593 9.33197 2.46198L10.0001 3.0621L10.6683 2.46206C11.6438 1.5861 12.8461 1.00033 14.0069 1C14.6628 1.00055 15.3116 1.12775 15.9159 1.37371C16.5202 1.61972 17.0673 1.97939 17.5263 2.4309Z"
-        stroke={isLike ? COLOR.RED : COLOR.CHARCOAL}
-        strokeWidth="2"
+        stroke={isLike ? COLOR.RED : strokeColor || COLOR.CHARCOAL}
+        strokeWidth={strokeWidth || '2'}
       />
     </svg>
   );
