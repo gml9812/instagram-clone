8f3b28cce5b52003c66109b3171be8c1953f71b3
devSominPark <min85472@gmail.com>
2022-11-25 10:27:01 +0900

    fix: CREATE_LIKE itemType에서 SUB_COMMENT 대신 COMMENT 사용

--- src/components/post/comment/SubCommentItem.tsx
+++ src/components/post/comment/SubCommentItem.tsx
@@ -32,10 +32,10 @@ const SubCommentItem = ({
     count: number;
   }>({ isLike, count: likeCount });
   const [createLike] = useMutation<{ createLike: boolean }>(CREATE_LIKE, {
-    variables: { likeInput: { itemId: id, type: 'SUB_COMMENT' } },
+    variables: { likeInput: { itemId: id, type: 'COMMENT' } },
   });
   const [deleteLike] = useMutation<{ deleteLike: boolean }>(DELETE_LIKE, {
-    variables: { likeInput: { itemId: id, type: 'SUB_COMMENT' } },
+    variables: { likeInput: { itemId: id, type: 'COMMENT' } },
   });
 
   const handleClickLike = async () => {
